4ebdd477f682ab34c8699c4a5d57ab66e9318a4f
0x534e92 <0x534e92@gmail.com>
2023-03-11 00:05:51 -0600

    add controllers for sign-in and sign-up

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_ancestry
+  belongs_to :user
+  has_many :notes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
+
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
