1032cfccdf0eb9463ed5932a659a5c36d42c44af
0x534e92 <0x534e92@gmail.com>
2023-03-31 21:13:08 -0500

    begin work on image uploading

--- app/javascript/Editor.js
+++ app/javascript/Editor.js
@@ -1,5 +1,5 @@
-import ReactQuill from "react-quill";
-import React, { useEffect, useRef, useState } from "react";
+import ReactQuill, { Quill } from "react-quill";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { Content } from "antd/es/layout/layout";
 import useNotes from "hooks/useNotes";
 import { updateContent } from "slices/notesSlice";
@@ -8,6 +8,29 @@ import { useDispatch } from "react-redux";
 const Editor = () => {
   const dispatch = useDispatch();
   const { selectedNoteId, content } = useNotes();
+
+  // const imageUploader = (file, callback) => {
+  //   console.log("file", file);
+  // };
+
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        // TODO: implement image upload
+        handlers: { image: () => console.log("hello") },
+        container: [
+          [{ header: [1, 2, false] }],
+          ["bold", "italic", "underline", "strike", "blockquote"],
+          ["link", "image"],
+          ["clean"],
+        ],
+      },
+      clipboard: {
+        matchVisual: false,
+      },
+    }),
+    []
+  );
 
   return (
     <Content
@@ -20,24 +43,14 @@ const Editor = () => {
     >
       <div style={{ backgroundColor: "#181818", color: "white", border: 0 }}>
         <ReactQuill
-          ref={(node) => {
-            // Oh my God, this took forever to figure out, but the reason wasn't that this code was wrong, but because the "key" wasn't set to a unique key, so it kept re-using the old DOM's value I think. After setting a key, this code finally ended up working. thank GOD!
-            // if (node != null) {
-            //   const len = node.unprivilegedEditor.getLength();
-            //   const selection = { index: len, length: len };
-            //   node.setEditorSelection(node.editor, selection);
-            // }
-            // Removing the above code since I don't think we want this functionality for now
-          }}
-          // TODO: fix bug where this isn't working with the new flow now
-          // I think it's because `selectedNoteId` on the second render isn't unique...
+          modules={modules}
           key={selectedNoteId}
           value={content}
           onChange={(content, delta, source, editor) => {
             dispatch(updateContent(content));
           }}
-          placeholder="Begin something amazing here..."
           scrollingContainer=".editor-container"
+          placeholder="Begin something amazing here..."
         />
       </div>
     </Content>
