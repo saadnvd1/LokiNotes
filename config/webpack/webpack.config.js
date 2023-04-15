b5cd1fb1bd2a83ae4da5d4eea4ef4b3fcff9ad3c
0x534e92 <0x534e92@gmail.com>
2023-04-15 06:53:56 -0500

    remove react-refresh into dev code block, not needed for prod

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,8 +1,9 @@
 const { webpackConfig, inliningCss } = require("shakapacker");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 if (isDevelopment && inliningCss) {
+  const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
   webpackConfig.plugins.push(
     new ReactRefreshWebpackPlugin({
       overlay: {
