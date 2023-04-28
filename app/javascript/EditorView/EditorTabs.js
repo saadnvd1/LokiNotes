90e216d4a2a7844069b8d02f1097621805260de8
0x534e92 <0x534e92@gmail.com>
2023-04-28 11:52:43 -0500

    restyle the tabs to look better and start adding a new button again

--- app/javascript/EditorView/EditorTabs.js
+++ app/javascript/EditorView/EditorTabs.js
@@ -3,39 +3,40 @@ import LBox from "components/LBox/LBox";
 import "EditorView/EditorTabs.css";
 import Editor from "EditorView/Editor";
 import { useDispatch, useSelector } from "react-redux";
-import { updateActiveIndex } from "slices/notesSlice";
+import { createNote, updateActiveIndex } from "slices/notesSlice";
 
 const EditorTabs = () => {
   const dispatch = useDispatch();
   const openTabs = useSelector((state) => state.notes.tabs.open);
   const activeIndex = useSelector((state) => state.notes.tabs.activeIndex);
+  const selectedNotebookId = useSelector(
+    (state) => state.notes.selectedNotebookId
+  );
+
+  const handleAddTab = () => {
+    dispatch(createNote({ notebookId: selectedNotebookId }));
+  };
 
   return (
     <LBox>
-      <LBox
-        flexRowStart
-        style={{
-          position: "sticky",
-          top: 0,
-          zIndex: 100,
-          backgroundColor: "white",
-        }}
-      >
+      <LBox flexRowStart className="editor-tab-bar">
         {openTabs.map((tab, index) => (
           <LBox
             onClick={() => dispatch(updateActiveIndex(index))}
             key={`${tab.noteId}-tab`}
           >
             <LBox
-              className="editor-tab"
-              style={{
-                backgroundColor: `${index === activeIndex ? "red" : "white"}`,
-              }}
+              className={`editor-tab ${
+                activeIndex === index ? "editor-tab-selected" : ""
+              }`}
             >
               Tab {index}
             </LBox>
           </LBox>
         ))}
+        <LBox onClick={handleAddTab}>
+          <LBox className="editor-tab">+</LBox>
+        </LBox>
       </LBox>
       {openTabs.map((tab, index) => (
         <LBox
